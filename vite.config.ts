20f9275d0b970e6b95843fb047b1ef5b4edadcd8
ferashatem <116522529+ferashatem@users.noreply.github.com>
2026-05-09 22:45:48 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'    // ← غيّر من 'vite' لـ 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/App.test.tsx',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
+})
